a08212812ba947756ab093fde35722ccf9d1110f
Rafal Korepta <rafal.korepta@gmail.com>
2021-01-13 16:51:59 +0100

    operator/api: Define RedpandaCluster CRD

--- apis/core/v1alpha1/redpandacluster_types.go
+++ apis/core/v1alpha1/redpandacluster_types.go
@@ -17,21 +17,33 @@ limitations under the License.
 // Package v1alpha1 represent Custom Resource definition of the vectorized.io core group
 package v1alpha1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 // RedpandaClusterSpec defines the desired state of RedpandaCluster
 type RedpandaClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of RedpandaCluster. Edit RedpandaCluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Version of Redpanda to use from upstream.
+	Version	string	`json:"version,omitempty"`
+	// Replicas determine how big the cluster will be.
+	// +kubebuilder:validation:Minimum=0
+	Replicas	*int32	`json:"replicas,omitempty"`
+	// Configuration represent redpanda specific configuration
+	Configuration	RedpandaConfig	`json:"configuration,omitempty"`
 }
 
 // RedpandaClusterStatus defines the observed state of RedpandaCluster
 type RedpandaClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Replicas show how many nodes are working in the cluster
+	// +optional
+	Replicas	int32	`json:"replicas,omitempty"`
+	// Nodes of the provision redpanda nodes
+	// +optional
+	Nodes	[]string	`json:"nodes,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -53,6 +65,30 @@ type RedpandaClusterList struct {
 	metav1.TypeMeta	`json:",inline"`
 	metav1.ListMeta	`json:"metadata,omitempty"`
 	Items		[]RedpandaCluster	`json:"items"`
+}
+
+// RedpandaConfig is the definition of the main configuration
+type RedpandaConfig struct {
+	RPCServer		SocketAddress	`json:"rpcServer,omitempty"`
+	AdvertisedRPCAPI	SocketAddress	`json:"advertisedRpcApi,omitempty"`
+	KafkaAPI		SocketAddress	`json:"kafkaApi,omitempty"`
+	AdvertisedKafkaAPI	SocketAddress	`json:"advertisedKafkaApi,omitempty"`
+	KafkaAPITLS		ServerTLS	`json:"kafkaApiTLS,omitempty"`
+	AdminAPI		SocketAddress	`json:"admin,omitempty"`
+	DeveloperMode		bool		`json:"developerMode,omitempty"`
+}
+
+// SocketAddress provide the way to configure the port
+type SocketAddress struct {
+	Port int `json:"port,omitempty"`
+}
+
+// ServerTLS allows the redpanda to configure TLS connection
+type ServerTLS struct {
+	KeyFile		string	`json:"keyFile,omitempty"`
+	CertFile	string	`json:"certFile,omitempty"`
+	TruststoreFile	string	`json:"truststoreFile,omitempty"`
+	Enabled		bool	`json:"enabled,omitempty"`
 }
 
 func init() {
